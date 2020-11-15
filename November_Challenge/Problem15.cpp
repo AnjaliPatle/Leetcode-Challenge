0769cdaca80a53d5041a6236db084629aeb2c26c
AnjaliPatle <anjalipatle1417@gmail.com>
2020-11-15 17:31:07 +0530

    Add Problem 15

---
+++ November_Challenge/Problem15.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int L, int R) {
+        if(root==NULL)return 0;
+        if(root->val<L){
+            return rangeSumBST(root->right,L,R);
+        }
+        else if(root->val>R){
+            return rangeSumBST(root->left,L,R);
+        }
+        else return root->val+rangeSumBST(root->right,L,R)+rangeSumBST(root->left,L,R);
+    }
+};

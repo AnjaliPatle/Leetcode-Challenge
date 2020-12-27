9245412f7fe7a61cf52e8d1c0097986b222dc29a
Anjali Patle <50516607+AnjaliPatle@users.noreply.github.com>
2020-12-27 23:25:54 +0530

    Add Problem 21

---
+++ December_Challenge/21.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int check(TreeNode* root){
+        if(root==NULL)return 0;
+        int lh=check(root->left);
+        if(lh==-1)return -1;
+        int rh=check(root->right);
+        if(rh==-1)return -1;
+        if(abs(lh-rh)>1)return -1;
+        return max(lh,rh)+1;
+    }
+    bool isBalanced(TreeNode* root) {
+        if(check(root)==-1)return false;
+        else return true;
+    }
+};

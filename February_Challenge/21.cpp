8c01cb0b2e0534d80592d56b579e7ab8f6753ca8
Anjali Patle <50516607+AnjaliPatle@users.noreply.github.com>
2021-02-21 20:43:46 +0530

    Add Problem 21

---
+++ February_Challenge/21.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int brokenCalc(int X, int Y) {
+        int ans=0;
+        while(Y>X){
+            if(Y%2==0){
+                Y/=2;
+            }
+            else Y++;
+            ans++;
+        }
+        return ans+X-Y;
+    }
+};

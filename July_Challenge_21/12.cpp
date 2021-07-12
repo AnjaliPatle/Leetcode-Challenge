ada181edce21ca9c6b93c4b2649a7280e3462c94
Anjali Patle <50516607+AnjaliPatle@users.noreply.github.com>
2021-07-12 22:52:36 +0530

    Add Problem 12

---
+++ July_Challenge_21/12.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char,int>mp1;
+        unordered_map<char,int>mp2;
+        for(int i=0;i<s.length(); i++){
+            if(mp1.find(s[i])==mp1.end() && mp2.find(t[i])==mp2.end()){
+                mp1[s[i]]=t[i];
+                mp2[t[i]]=s[i];
+            }
+            else if(mp1[s[i]]!=t[i] || mp2[t[i]]!=s[i])
+                return false;
+        }
+        return true;
+    }
+};
